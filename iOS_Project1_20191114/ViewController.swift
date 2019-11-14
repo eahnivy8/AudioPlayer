364d5c1df07d365834acfaccddc68f60bb35e11e
Eddie <eahnivy8@gmail.com>
2019-11-14 15:49:17 +0900

    Initial Commit

---
+++ iOS_Project1_20191114/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Project1_20191114
+//
+//  Created by Eddie Ahn on 2019/11/14.
+//  Copyright © 2019 Eddie Ahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
